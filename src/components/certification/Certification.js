7f4edf7a7fb014331b203d89e408023081ed4840
Saurabhrithe <715saurabh@gmail.com>
2025-01-23 23:27:44 +0530

    modified data

--- src/components/certification/Certification.js
+++ src/components/certification/Certification.js
@@ -6,12 +6,11 @@ export default function Certification() {
     <div className="certifications" id="certification">
         <h3 className="certification-header"> Certifications and Awards</h3>
         <div className="boxA">
-        <ul>
-            <li>&bull; Azure Data Fundamentals(DP-900) <a href='https://www.credly.com/badges/c4300a90-0d45-4424-abb7-f6bd48fe04c9'>&#128279;</a></li>
-            <li>&bull; GitHub Foundations <a href="https://www.credly.com/badges/5f2e3489-a67e-40ef-abcb-11e3ad21d235">&#128279;</a></li>
-            <li>&bull; Promising New Star (Associated with cognizant, Nov-2023)</li>
-            <li>&bull; Always striving, never settling (Associated with cognizant, May-2024)</li>
-        </ul> 
+        <p> &bull; Azure Data Fundamentals(DP-900)<a href='https://www.credly.com/badges/c4300a90-0d45-4424-abb7-f6bd48fe04c9'>&#128279;</a></p> 
+             <p> &bull; GitHub Foundations <a href="https://www.credly.com/badges/5f2e3489-a67e-40ef-abcb-11e3ad21d235">&#128279;</a> </p>
+             <p> &bull; Promising New Star (Associated with cognizant, Nov-2023)</p>
+             <p> &bull; Always striving, never settling (Associated with cognizant, May-2024)</p>
+      
         </div> 
     </div>
   )
